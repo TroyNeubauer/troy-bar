2a8d3fbb11c590ca19ec5603d9822f007ac7352d
Troy Neubauer <troyneubauer@gmail.com>
2020-09-13 09:49:19 -0400

    Got rust to set taskbar text from X11 bindings!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+
+extern crate x11;
+use std::ptr;
+use std::ffi::CStr;
+
+#[link(name = "X11")]
+extern {}
+
 fn main() {
-    println!("Hello, world!");
+	println!("Hello, world!");
+	unsafe {
+		let display = x11::xlib::XOpenDisplay(ptr::null());
+		let screen = x11::xlib::XDefaultScreen(display);
+		let root = x11::xlib::XRootWindow(display, screen);
+		x11::xlib::XStoreName(display, root, CStr::from_bytes_with_nul(b"Rust is great!\0").unwrap().as_ptr());
+
+		x11::xlib::XCloseDisplay(display);
+	}
+	println!("Set window title");
+
 }
