77d80aa1b2b6494e78feb0dced84d8816350ed67
Troy Neubauer <troyneubauer@gmail.com>
2020-09-13 09:57:24 -0400

    abstracted x11 code to util.rs

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,8 @@
-
-extern crate x11;
-use std::ptr;
-use std::ffi::CStr;
-
-#[link(name = "X11")]
-extern {}
+mod util;
 
 fn main() {
 	println!("Hello, world!");
-	unsafe {
-		let display = x11::xlib::XOpenDisplay(ptr::null());
-		let screen = x11::xlib::XDefaultScreen(display);
-		let root = x11::xlib::XRootWindow(display, screen);
-		x11::xlib::XStoreName(display, root, CStr::from_bytes_with_nul(b"Rust is great!\0").unwrap().as_ptr());
-
-		x11::xlib::XCloseDisplay(display);
-	}
+	util::set_window_title(b"Rust is great 2\0");
 	println!("Set window title");
 
 }
---
+++ src/util.rs
@@ -0,0 +1,19 @@
+
+extern crate x11;
+use std::ptr;
+use std::ffi::CStr;
+
+#[link(name = "X11")]
+extern {}
+
+pub fn set_window_title(bytes: &[u8]) {
+	unsafe {
+		let display = x11::xlib::XOpenDisplay(ptr::null());
+		let screen = x11::xlib::XDefaultScreen(display);
+		let root = x11::xlib::XRootWindow(display, screen);
+		x11::xlib::XStoreName(display, root, CStr::from_bytes_with_nul(bytes).unwrap().as_ptr());
+
+		x11::xlib::XCloseDisplay(display);
+	}
+
+}
