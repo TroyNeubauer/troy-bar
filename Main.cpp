b7d79d7976c930cac884f342c0f76eca721e0d95
Troy Neubauer <troyneubauer@gmail.com>
2020-09-12 20:34:17 -0400

    Finished C workspace and inited Rust stuff

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,35 @@
-#include <iostream>
 
-int main()
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <X11/Xatom.h>
+#include <X11/Xmu/CurUtil.h>
+#include <X11/Xcursor/Xcursor.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+//#include "X11/bitmaps/gray"
+
+
+int main(int argc, char *argv[]) 
 {
-    std::cout << "Hello World!" << std::endl;
+	const char *display_name = nullptr;
+
+	char* program_name=argv[0];
+
+	Display* dpy = XOpenDisplay(display_name);
+	if (!dpy)
+	{
+		fprintf(stderr, "%s:  unable to open display '%s'\n", program_name, XDisplayName (display_name));
+		exit(2);
+	}
+	int screen = DefaultScreen(dpy);
+	Window root = RootWindow(dpy, screen);
+
+	XStoreName(dpy, root, "adads");  
+  
+	XCloseDisplay(dpy);
+	exit (0);
 }
+
+
 
--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@ SRCS = Main.cpp
 OBJS = $(subst .cc,.o,$(SRCS))
 
 troybar: $(OBJS)
-	g++ $(LDFLAGS) -o troybar $(OBJS) $(LDLIBS)
+	g++ $(LDFLAGS) -o troybar $(OBJS) $(LDLIBS) -lX11
 
 main.o: Main.cpp
 	g++ $(CPPFLAGS) Main.cpp
