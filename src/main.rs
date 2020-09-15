ef14dc118615fef3707d6f54b90a69ec09473a9c
Troy Neubauer <troyneubauer@gmail.com>
2020-09-15 10:39:47 -0400

    fixed ci

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,29 @@ mod open_weather_map;
 
 use chrono::{Local, Utc};
 
-use std::{thread, time};
+use std::{thread, time, env};
 
 
 fn main() {
+	let str_args: Vec<String> = env::args().collect();
+	if str_args.len() != 4 {
+		println!("Expected 3 args but got {}", str_args.len());
+		panic!("USAGE: troybar latitude longitude openweathermap.org-key");
+	}
+	let args = open_weather_map::Arguments {
+		latitude:  str_args[1].parse::<f64>().unwrap(),
+		longitude: str_args[2].parse::<f64>().unwrap(),
+		api_key:  str_args[3].clone(),
+	};
+
+
 	let mut s = String::new();
 	let mut data: open_weather_map::Data = open_weather_map::Data::new();
 
 	let mut next_sec = (Utc::now().timestamp_millis() / 1000) * 1000 + 1000;//TODO figure out how to use the time library properly so we can avoid nasty math
 	loop {
 		s.clear();
-		open_weather_map::update(&mut data);
+		open_weather_map::update(&mut data, &args);
 
 		bar_weather(&mut s, &mut data);
 		separator(&mut s);
@@ -44,6 +56,5 @@ fn bar_time(s: &mut String) {
 }
 
 fn bar_weather(s: &mut String, data: &mut open_weather_map::Data) {
-	open_weather_map::update(data);
 	s.push_str(format!("{:.1}°F @{:.0}% : {:.0}-{:.0}°F {}-{} T{:.0}% H{:.0}%", data.now, data.humidity, data.low, data.high, data.sunrise.format("%H:%M"), data.sunset.format("%H:%M"), data.today_precipitation_percent, data.hour_precipitation_percent).as_str());
 }
--- src/open_weather_map.rs
+++ src/open_weather_map.rs
@@ -12,6 +12,12 @@ pub struct Data {
 	last_update: chrono::DateTime<Utc>,
 }
 
+pub struct Arguments {
+	pub latitude: f64,
+	pub longitude: f64,
+	pub api_key: String,
+}
+
 impl Data {
 	pub fn new() -> Data {
 	
@@ -30,18 +36,16 @@ impl Data {
 }
 
 
-static OPEN_WEATHER_MAP_KEY: &str = include_str!("secrets/open_weather_map_key.txt");
-
 //static mut 
 
-pub fn update(data: &mut Data) {
+pub fn update(data: &mut Data, args: &Arguments) {
 
 	let weather_update_rate = Duration::seconds(90);//They allow 1000 requests per day on the free tier: = 86.4 seconds between request to avoid getting rate limited
 	let difference = Utc::now().signed_duration_since(data.last_update);
 	if difference > weather_update_rate {
-		println!("Updating...");
 		data.last_update = Utc::now();
-		request_data(data);
+		request_data(data, args);
+		println!("Weather data updated");
 	}
 }
 
@@ -49,39 +53,40 @@ fn kelvin_to_f(k: f64) -> f64 {
 	(k - 273.15) * 9.0/5.0 + 32.0
 }
 
-pub struct Position {
-	latitude: f32,
-	longitude: f32,
-}
-
-pub fn get_position() -> Position {
-	//TODO: gather the user's location automatically instead of hard-coding it
-	include!("secrets/location.in")
-}
-
-
-fn request_data(data: &mut Data) {
-	let pos = get_position();
-	let req_url = format!("https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&appid={}", pos.latitude, pos.longitude, OPEN_WEATHER_MAP_KEY);
-	println!("Requesting {}", req_url);
+fn request_data(data: &mut Data, args: &Arguments) {
+	let req_url = format!("https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&appid={}", args.latitude, args.longitude, args.api_key);
 	let response = reqwest::blocking::get(req_url.as_str()).unwrap().text().unwrap();
-	println!("Got {}", response);
 
 	let result = json::parse(response.as_str()).unwrap();
-	let current = &result["current"];
-	let hour = &result["hourly"][0];
-	let today = &result["daily"][0];
-
-	data.high = kelvin_to_f(today["temp"]["max"].as_f64().unwrap()) as f32;
-	data.low = kelvin_to_f(today["temp"]["min"].as_f64().unwrap()) as f32;
-	data.humidity = current["humidity"].as_f64().unwrap() as f32;
-
-	data.now = kelvin_to_f(current["temp"].as_f64().unwrap()) as f32;
-
-	data.sunrise = Local.timestamp(current["sunrise"].as_i64().unwrap(), 0);
-	data.sunset = Local.timestamp(current["sunset"].as_i64().unwrap(), 0);
-	data.today_precipitation_percent = today["pop"].as_f64().unwrap() as f32;
-	data.hour_precipitation_percent = hour["pop"].as_f64().unwrap() as f32;
+	if result["message"].as_str().is_some() {
+		println!("Failed to request weather data!");
+		println!("Code {}, message {}", result["cod"].as_u32().unwrap(), result["message"].as_str().unwrap());
+		data.high = 0.0;
+		data.low = 0.0;
+		data.humidity = 0.0;
 	
+		data.now = 0.0;
+	
+		data.sunrise = Local.timestamp(0, 0);
+		data.sunset = Local.timestamp(0, 0);
+		data.today_precipitation_percent = 0.0;
+		data.hour_precipitation_percent = 0.0;
+	} else {
+		let current = &result["current"];
+		let hour = &result["hourly"][0];
+		let today = &result["daily"][0];
+	
+		data.high = kelvin_to_f(today["temp"]["max"].as_f64().unwrap()) as f32;
+		data.low = kelvin_to_f(today["temp"]["min"].as_f64().unwrap()) as f32;
+		data.humidity = current["humidity"].as_f64().unwrap() as f32;
+	
+		data.now = kelvin_to_f(current["temp"].as_f64().unwrap()) as f32;
+	
+		data.sunrise = Local.timestamp(current["sunrise"].as_i64().unwrap(), 0);
+		data.sunset = Local.timestamp(current["sunset"].as_i64().unwrap(), 0);
+		data.today_precipitation_percent = today["pop"].as_f64().unwrap() as f32;
+		data.hour_precipitation_percent = hour["pop"].as_f64().unwrap() as f32;
+	
+	}	
 }
 
